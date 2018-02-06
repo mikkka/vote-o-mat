8c1757d4b1487116adaa543c15132830685623a4
mick <mick@z-inc.net>
2018-02-07 00:07:40 +0500

    create vote state types

--- src/main/scala/name/mtkachev/voteomat/domain/VoteState.scala
+++ src/main/scala/name/mtkachev/voteomat/domain/VoteState.scala
@@ -1,9 +1,78 @@
 package name.mtkachev.voteomat.domain
 
-trait VoteState
-trait Response
+import java.time.LocalDateTime
 
-//state monad?
-object VoteState {
-  def change(state: VoteState, cmd: Command): (VoteState, Response) = ???
+case class VoteState (
+  votings: Map[Int,Voting]
+)
+
+case class User (name: String)
+
+case class Voting (
+  id: Int,
+  owner: User,
+  name: String,
+  isAnonymous: Boolean,
+  resultsAfterStop: Boolean,
+  startDate: Option[LocalDateTime],
+  stopDate: Option[LocalDateTime],
+  questions: List[Question]
+)
+
+sealed trait Answer {
+  def author: Option[User]
+  def timeDate: LocalDateTime
 }
+
+sealed trait Question {
+  type A <: Answer
+
+  def id: Int
+  def label: String
+  def answers: List[A]
+}
+
+case class OpenQuestion (
+  override val id: Int,
+  override val label: String,
+  override val answers: List[OpenAnswer]
+) extends Question {
+  type A = OpenAnswer
+}
+
+case class OpenAnswer (
+  override val author: Option[User],
+  override val timeDate: LocalDateTime,
+  value: String
+) extends Answer
+
+
+case class CloseQuestion (
+  override val id: Int,
+  override val label: String,
+  options: List[String],
+  override val answers: List[CloseAnswer]
+) extends Question {
+  type A = CloseAnswer
+}
+
+case class CloseAnswer (
+  override val author: Option[User],
+  override val timeDate: LocalDateTime,
+  value: Int
+) extends Answer
+
+case class MultiQuestion (
+  override val id: Int,
+  override val label: String,
+  options: List[String],
+  override val answers: List[MultiAnswer]
+) extends Question {
+  type A = MultiAnswer
+}
+
+case class MultiAnswer (
+  override val author: Option[User],
+  override val timeDate: LocalDateTime,
+  value: Set[Int]
+) extends Answer
