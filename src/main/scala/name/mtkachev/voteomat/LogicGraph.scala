ed640ca84dd06a97978b55dd3845ddbbece87d2a
mick <mick@z-inc.net>
2018-02-15 00:13:11 +0500

    move out logic graph

--- src/main/scala/name/mtkachev/voteomat/LogicGraph.scala
+++ src/main/scala/name/mtkachev/voteomat/LogicGraph.scala
@@ -1,53 +1,14 @@
 package name.mtkachev.voteomat
 
-import java.time.LocalDateTime
-
 import akka.NotUsed
-import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, FlowShape}
-import akka.stream.scaladsl.{Broadcast, Flow, Framing, GraphDSL, Merge, Sink, Source, Tcp}
-import akka.stream.scaladsl.Tcp.{IncomingConnection, ServerBinding}
-import akka.util.ByteString
+import akka.stream.FlowShape
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Source}
 import name.mtkachev.voteomat.domain._
 
-import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
-object TelnetApp {
-  def main(args: Array[String]): Unit = {
-    println("hello!")
-    implicit val actorSystem = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-
-    val stateCtx = new VoteStateCtx
-
-    val connections: Source[IncomingConnection, Future[ServerBinding]] =
-      Tcp().bind("127.0.0.1", 8888)
-
-    connections.runForeach { connection =>
-      val sessionCtx = new SessionCtx
-
-      import connection._
-      val welcomeMsg =
-        s"Welcome to: $localAddress, you is is: $remoteAddress! Predstavsya mraz: "
-      val welcome = Source.single(welcomeMsg)
-
-      val g: Flow[String, String, NotUsed] = Flow.fromGraph(createLogicGrapth(stateCtx, sessionCtx, welcome))
-
-      val logic = Flow[ByteString]
-        .via(
-          Framing.delimiter(ByteString("\n\n"),
-            maximumFrameLength = 256,
-            allowTruncation = true))
-        .map(_.utf8String)
-        .via(g).map(x => ByteString(x + "\n" + ("-" * 30) + "\n"))
-
-
-      connection.handleWith(logic)
-    }
-  }
-
-  private def createLogicGrapth(stateCtx: VoteStateCtx, sessionCtx: SessionCtx, welcome: Source[String, NotUsed]) = {
+object LogicGraph {
+  def apply(stateCtx: VoteStateCtx, sessionCtx: SessionCtx, welcome: Source[String, NotUsed]) = {
     val commandParser = new SimpleCommandParser {}
     val resultsRenderer = new ResultsRendererImpl
     GraphDSL.create() { implicit b =>
